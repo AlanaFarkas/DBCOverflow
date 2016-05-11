8acaa5cd4027c3d363b53a7a3fedce9bec89db94
John Colella <colella.john@gmail.com>
2016-05-11 19:17:41 -0400

    continue working on question controller and views

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -8,7 +8,6 @@ get '/questions/new' do
 end
 
 post '/questions' do
-  binding.pry
   @question = Question.new(title: params[:title], body: params[:body], user_id: current_user.id)
   tags = params[:tags].split(", ")
 
@@ -21,6 +20,7 @@ post '/questions' do
         new_tag = Tag.create(name: tag)
         @questions << new_tag
       end
+    end
     redirect "/questions/#{@question.id}"
   else
     @errors = @question.errors.full_messages
@@ -35,16 +35,19 @@ get '/questions/:id' do
 end
 
 get '/questions/:id/edit' do
-
+  @question = Question.find_by(id: params[:id])
   erb :'/questions/edit'
 end
 
 put '/questions/:id' do
+  @question = Question.find_by(id: params[:id])
+  @question.update_attributes(body: params[:body])
 
-  redirect "/questions/#{question.id}"
+  redirect "/questions/#{@question.id}"
 end
 
 delete '/questions/:id' do
-
+  question = Question.find_by(id: params[:id])
+  question.destroy
   redirect '/'
 end
